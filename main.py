af530bbce92b7464f8bd5cabb900b7376feec411
Christian Huyghe <huyghecr@gmail.com>
2023-10-13 14:08:50 -0400

    Formatting, comments

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import asyncio
 import websockets as ws
 from websockets import server
 
+# In the current state, hosts a localhost websocket on port 8765.
+# This connection accepts messages from the client and echoes them back.
 async def connection(websocket: server.WebSocketServerProtocol):
     try:
         print("New Connection:", websocket.id)
@@ -9,8 +11,10 @@ async def connection(websocket: server.WebSocketServerProtocol):
             message = await websocket.recv()
             print(message)
             await websocket.send(message)
+
     except ws.ConnectionClosedOK:
         print("Client disconnected:", websocket.id)
+
     except ConnectionError:
         print("Client disconnected with error:", websocket.id)
 
