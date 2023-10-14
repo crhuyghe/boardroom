14ea36bdb9a963138e64e6d638edca73246f9d77
Christian Huyghe <huyghecr@gmail.com>
2023-10-14 13:18:32 -0400

    Changed main.py to interpret message types

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 import websockets as ws
 from websockets import server
 from Classes.DatabaseReader import DatabaseReader
@@ -7,47 +8,49 @@ BoardroomDB = DatabaseReader("boardroom")
 UserDB = DatabaseReader("user")
 MessageDB = DatabaseReader("message")
 
+decoder = json.JSONDecoder()
+
 # In the current state, hosts a localhost websocket on port 8765.
 # This connection accepts messages from the client and echoes them back.
 async def connection(websocket: server.WebSocketServerProtocol):
     try:
         print("New Connection:", websocket.id)
         while True:
-            message = await websocket.recv()
+            message = decoder.decode(await websocket.recv())
 
-            if message == 0:
+            if message["action"] == 0:
                 print("AccessAccount")
-            elif message == 1:
+            elif message["action"] == 1:
                 print("CreateAccount")
-            elif message == 2:
+            elif message["action"] == 2:
                 print("CreatePost")
-            elif message == 3:
+            elif message["action"] == 3:
                 print("DeleteAccount")
-            elif message == 4:
+            elif message["action"] == 4:
                 print("DeleteMessage")
-            elif message == 5:
+            elif message["action"] == 5:
                 print("EditMessage")
-            elif message == 6:
+            elif message["action"] == 6:
                 print("GetPost")
-            elif message == 7:
+            elif message["action"] == 7:
                 print("LikePost")
-            elif message == 8:
+            elif message["action"] == 8:
                 print("LikePostReply")
-            elif message == 9:
+            elif message["action"] == 9:
                 print("LogoutAccount")
-            elif message == 10:
+            elif message["action"] == 10:
                 print("ModifyAccount")
-            elif message == 11:
+            elif message["action"] == 11:
                 print("ModifyPost")
-            elif message == 12:
+            elif message["action"] == 12:
                 print("ModifyPostReply")
-            elif message == 13:
+            elif message["action"] == 13:
                 print("Refresh")
-            elif message == 14:
+            elif message["action"] == 14:
                 print("ReplyPost")
-            elif message == 15:
+            elif message["action"] == 15:
                 print("SearchPosts")
-            elif message == 16:
+            elif message["action"] == 16:
                 print("SendMessage")
 
             print(message)
