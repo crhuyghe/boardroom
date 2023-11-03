3eb11bb7f41927f7bf4b6a0adbb6ed4df6682374
Christian Huyghe <huyghecr@gmail.com>
2023-11-03 14:20:57 -0400

    Added basic events to main.js

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-import "JSON Templates"
+// import "JSON Templates"
 
 const ws = new WebSocket("ws://localhost:8765")
 
 ws.onopen = event =>  // Send a message to the server upon initial opening of the socket.
 {
-    alert("Connection established.")
-    alert("Sending message to server...")
-    ws.send("This message has been sent to the server.")
+    console.log("Connection established.")
+    console.log("Sending message to server...")
+    ws.send(JSON.stringify({"action": 1}))
 }
 
+ws.onerror = error =>
+{
+    console.error(error)
+}
 
+ws.onmessage = event =>
+{
+    console.log(event.data)
+}
 // ws.close()  // Close the socket.
 
 
