f84f6828aefac0f5e465b5759fa5cf4f79fa0a75
Christian Huyghe <huyghecr@gmail.com>
2023-11-25 13:19:29 -0500

    Fixed coloring

--- Frontend/Classes/ResizingText.py
+++ Frontend/Classes/ResizingText.py
@@ -9,6 +9,7 @@ class ResizingText(ttk.Frame):
                  dark_mode: bool = False, alt_color=False, dynamic=False, text_padding=0, display_text="", **kwargs):
         super().__init__(master, **kwargs)
         self.dark_mode = dark_mode
+        self.alt_color = alt_color
         self.editing_enabled = False
         self.display_text = display_text
         self._dynamic = dynamic
@@ -72,17 +73,24 @@ class ResizingText(ttk.Frame):
     def swap_mode(self):
         self.dark_mode = not self.dark_mode
         if self.dark_mode:
-            bg = "#24272b"
+            if self.alt_color:
+                bg = "#292c30"
+            else:
+                bg = "#24272b"
             fg = "#b6bfcc"
             ins_bg = "#b6bfcc"
         else:
-            bg = "#FFFFFF"
+            if self.alt_color:
+                bg = "#EEEEEE"
+            else:
+                bg = "#FFFFFF"
             fg = "#000000"
             ins_bg = "#000000"
         self.text_widget.configure(bg=bg, fg=fg, insertbackground=ins_bg)
 
     def get_text(self):
-        if self._empty:
+        fg = self.text_widget.cget("foreground")
+        if self._empty or fg == "#888888" or fg == "#666666":
             return ""
         else:
             return self.text_widget.get(1.0, "end")
@@ -102,34 +110,48 @@ class ResizingText(ttk.Frame):
 
     def toggle_modification(self):
         self.editing_enabled = not self.editing_enabled
+        if self.dark_mode:
+            if self.alt_color:
+                bg = "#292c30"
+                alt_bg = "#24272b"
+            else:
+                bg = "#24272b"
+                alt_bg = "#292c30"
+            fg = "#b6bfcc"
+        else:
+            if self.alt_color:
+                bg = "#EEEEEE"
+                alt_bg = "#FFFFFF"
+            else:
+                bg = "#FFFFFF"
+                alt_bg = "#EEEEEE"
+            fg = "#000000"
         if self.editing_enabled:
             if not self._dynamic:
                 self.scrollbar.pack(side="right", expand=1, fill="y")
                 self._update_size(6)
-            self.text_widget.configure(state="normal", bd=1)
+            self.text_widget.configure(state="normal", bg=alt_bg)
             self.check_display_text()
         else:
             if not self._dynamic:
                 self.scrollbar.pack_forget()
             if self._empty:
                 self.change_text("")
-                if self.dark_mode:
-                    self.text_widget.configure(foreground="#b6bfcc")
-                else:
-                    self.text_widget.configure(foreground="#000000")
-            self.text_widget.configure(state="disabled", bd=0)
+                self.text_widget.configure(foreground=fg)
+            self.text_widget.configure(state="disabled", bg=bg)
             self._update_size()
 
     def check_display_text(self):
         if self.editing_enabled and self.text_widget.get(1.0, "end") == "\n" and self.focus_get() != self.text_widget:
             if self.dark_mode:
-                self.text_widget.configure(foreground="#AAAAAA")
+                self.text_widget.configure(foreground="#888888")
             else:
-                self.text_widget.configure(foreground="#444444")
+                self.text_widget.configure(foreground="#666666")
             self.text_widget.insert(1.0, self.display_text)
 
     def hide_display_text(self):
-        if self.editing_enabled and self._empty:
+        fg = self.text_widget.cget("foreground")
+        if self.editing_enabled and (self._empty or fg == "#888888" or fg == "#666666"):
             self.text_widget.delete(1.0, "end")
             if self.dark_mode:
                 self.text_widget.configure(foreground="#b6bfcc")
