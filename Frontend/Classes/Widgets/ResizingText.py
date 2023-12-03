9cbcc64aa86dbcffec99fa30a1e4ab7563e15f22
Christian Huyghe <huyghecr@gmail.com>
2023-12-03 08:51:11 -0500

    fixed swap mode into light mode

--- Frontend/Classes/Widgets/ResizingText.py
+++ Frontend/Classes/Widgets/ResizingText.py
@@ -90,9 +90,9 @@ class ResizingText(ttk.Frame, DarkMode):
             ins_bg = "#b6bfcc"
         else:
             if self.alt_color:
-                bg = "#EEEEEE"
-            else:
                 bg = "#FFFFFF"
+            else:
+                bg = "#EEEEEE"
             fg = "#000000"
             ins_bg = "#000000"
         self.text_widget.configure(bg=bg, fg=fg, insertbackground=ins_bg)
